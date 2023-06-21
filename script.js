d27c200bda28db368ab9e20401fc070746cd245e
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-21 10:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+ if (localStorage.getItem("username") && localStorage.getItem("password")) {
+      var existingButton = document.createElement("button");
+      existingButton.id = "existing";
+      existingButton.textContent = "Login as existing user";
+      document.body.appendChild(existingButton);
+      
+      existingButton.addEventListener("click", function() {
+        var savedUsername = localStorage.getItem("username");
+        alert("Logged in as " + savedUsername);
+      });
+    }
+    
+    // Login form submission handler
+    var loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+      
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+      var rememberMe = document.getElementById("rememberMe").checked;
+      
+      if (rememberMe) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+      }
+      
+      alert("Logged in as " + username);
+    });
